3ee084a906e5c5f2f76a6d550089c1b2d2e323fa
Leonid Shevtsov <leonid@shevtsov.me>
2012-01-28 00:17:39 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiqpayDemo::Application.config.secret_token = '03bd664dcb52a267f5118efcd04a13cd626e54925eadaf54ca31fc4685f9c6b4cdd792bf5c6d4165493ea6ee93122bb8804894ec3bf560bd9b63b43c7347ce88'
